7696596881c20cb6e399fe6fed7c20577cbc6528
Ivan Grimaldo <ivan@grimaldo.dev>
2019-11-02 10:52:32 -0600

    Add users service

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ var faker = require('faker');
 
 const data = {
   posts: [],
-  users:[]
+  users:[],
+  comments: []
+}
+
+for (let u = 0; u < 10; u++) {
+  data.users.push({
+    _id: faker.random.uuid(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  })
+}
+
+for (let i = 0; i < 10; i++) {
+  data.posts.push({
+    _id: faker.random.uuid(),
+    content: faker.lorem.paragraph(),
+    creation: faker.date.past()
+  })
+}
+
+for(let j = 0; j < 20; j++) {
+  data.comments.push({
+    _id: faker.random.uuid(),
+    content: faker.lorem.paragraph(),
+    creation: faker.date.past(),
+    parent: faker.random.uuid()
+  })
 }
 
 module.exports = function () {
-  for (let i = 0; i < 20; i++) {
-    data.posts.push({
-      _id: faker.random.uuid(),
-      content: faker.lorem.paragraph(),
-      creation: faker.date.past()
-    })
-  }
   return data;
 }
+
